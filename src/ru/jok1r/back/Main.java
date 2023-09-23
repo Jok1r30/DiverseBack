fddc37fcefda2d05067ad346fbfe0aa766bdc9c8
jok1r30 <jok1r30@yandex.ru>
2023-09-23 14:10:30 +0300

    Some optimizations

--- src/ru/jok1r/back/Main.java
+++ src/ru/jok1r/back/Main.java
@@ -7,6 +7,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -15,7 +16,7 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements CommandExecutor {
 
     public Map<String, Location> backMap = Maps.newHashMap();
     public List<String> usesTp = Lists.newArrayList();
@@ -23,13 +24,13 @@ public class Main extends JavaPlugin {
     public File file = new File(this.getDataFolder(), "config.yml");
 
     public void onEnable() {
-        if (!this.file.exists()) {
-            this.saveDefaultConfig();
-            this.getConfig().options().copyDefaults(true);
+        if(!this.file.exists()) {
+            this.saveResource("config.yml", false);
         }
 
         this.getLogger().severe(">> Плагин на телепортацию до последней точки смерти игрока, автор Jok1r, vk.com/jok1rstyle, vk.com/diverselab");
         this.getServer().getPluginManager().registerEvents(new PlayerDeathListener(this), this);
+        this.getCommand("dback").setExecutor(this);
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
@@ -43,29 +44,29 @@ public class Main extends JavaPlugin {
             if (!player.isOp() && !player.hasPermission("diverseBack.use")) {
                 this.sendMessageToPlayer(player, this.getConfig().getString("messages.noPerm"));
             } else {
-                if (usesTp.contains(player.getDisplayName())) {
+                if (usesTp.contains(player.getName())) {
                     this.sendMessageToPlayer(player, this.getConfig().getString("messages.cantTp"));
                     return false;
                 }
 
-                if (!backMap.containsKey(player.getDisplayName())) {
+                if (!backMap.containsKey(player.getName())) {
                     this.sendMessageToPlayer(player, this.getConfig().getString("messages.noDeath"));
                     return false;
                 }
 
-                if (cooldowns.containsKey(player.getDisplayName()) && System.currentTimeMillis() < cooldowns.get(player.getDisplayName())) {
+                if (cooldowns.containsKey(player.getName()) && System.currentTimeMillis() < cooldowns.get(player.getName())) {
                     String cdMsg = this.getConfig().getString("messages.cooldown");
-                    long cd = (cooldowns.get(player.getDisplayName()) - System.currentTimeMillis()) / 1000L;
+                    long cd = (cooldowns.get(player.getName()) - System.currentTimeMillis()) / 1000L;
                     cdMsg = cdMsg.replaceAll("\\%time\\%", cd + "");
                     this.sendMessageToPlayer(player, cdMsg);
                     return false;
                 }
 
-                Location loc = backMap.get(player.getDisplayName());
+                Location loc = backMap.get(player.getName());
                 player.teleport(loc);
-                usesTp.add(player.getDisplayName());
+                usesTp.add(player.getName());
                 this.sendMessageToPlayer(player, this.getConfig().getString("messages.teleport"));
-                cooldowns.put(player.getDisplayName(), System.currentTimeMillis() + (this.getConfig().getInt("cooldown") * 1000L));
+                cooldowns.put(player.getName(), System.currentTimeMillis() + (this.getConfig().getInt("cooldown") * 1000L));
             }
         }
 
